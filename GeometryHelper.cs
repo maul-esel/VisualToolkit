94b3175fce75d834998f239afc3151f3b22b349c
maul.esel <maul.esel@go4more.de>
2014-10-06 20:56:09 +0200

    add static GeometryHelper class

---
+++ GeometryHelper.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace VisualToolkit
+{
+	public static class GeometryHelper
+	{
+		public static int GetDimension(Rectangle rect, Orientation orientation)
+		{
+			return GetDimension(rect.Size, orientation);
+		}
+
+		public static int GetDimension(Size size, Orientation orientation)
+		{
+			switch (orientation) {
+				case Orientation.Horizontal:
+					return size.Width;
+				case Orientation.Vertical:
+					return size.Height;
+			}
+			throw new NotSupportedException();
+		}
+	}
+}
