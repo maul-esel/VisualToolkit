a35e0690b9db72850dfd8f999c3958dbf15438c5
maul.esel <maul.esel@go4more.de>
2014-10-07 19:36:23 +0200

    add a ButtonBase and a Button class

---
+++ Button.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace VisualToolkit
+{
+	public class Button : ButtonBase
+	{
+		public Button()
+			: base()
+		{
+			Font = new Font(Font.FontFamily, 1.25f * Font.Size);
+
+			ForeColor = ColorTheme.DefaultTheme.TextColor;
+			PressedForeColor = ColorTheme.HighlightTheme.TextColor;
+		}
+
+		protected override Size DefaultSize {
+			get { return new Size(100, 40); }
+		}
+
+		protected override void OnTextChanged(EventArgs e)
+		{
+			Size minSize = TextRenderer.MeasureText(Text, Font);
+			Size = new Size(
+				Math.Max(minSize.Width + Padding.Horizontal, Size.Width),
+				Math.Max(minSize.Height + Padding.Vertical,  Size.Height)
+			);
+			base.OnTextChanged(e);
+		}
+
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			Rectangle rect = GeometryHelper.ApplyPadding(ClientRectangle, Padding);
+			if (IsPressed)
+				rect = GeometryHelper.ApplyPadding(rect, PressExpansion);
+
+			StringFormat format = new StringFormat() {
+				Alignment = StringAlignment.Center,
+				LineAlignment = StringAlignment.Center
+			};
+
+			using (Pen borderPen = new Pen(IsPressed ? PressedBorderColor : BorderColor, BorderWidth))
+				e.Graphics.DrawRectangle(borderPen, rect);
+
+			using (Brush fgBrush = new SolidBrush(IsPressed ? PressedForeColor : ForeColor))
+				e.Graphics.DrawString(Text, Font, fgBrush, rect, format);
+
+			base.OnPaint(e);
+		}
+
+		protected override void OnPaintBackground(PaintEventArgs e)
+		{
+			base.OnPaintBackground(e);
+			using (Brush brush = new SolidBrush(IsPressed ? PressedBackColor : BackColor))
+				e.Graphics.FillRectangle(brush, IsPressed ? ClientRectangle : GeometryHelper.ApplyPadding(ClientRectangle, PressExpansion));
+		}
+	}
+}
---
+++ ButtonBase.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace VisualToolkit
+{
+	public abstract class ButtonBase : Control, IButtonControl
+	{
+		public ButtonBase()
+			: base()
+		{
+			BorderWidth = 2;
+			Padding = new Padding(1);
+
+			BackColor = ColorTheme.DefaultTheme.BackColor;
+			BorderColor = ColorTheme.DefaultTheme.BorderColor;
+
+			PressedBackColor = ColorTheme.HighlightTheme.BackColor;
+			PressedBorderColor = ColorTheme.HighlightTheme.BorderColor;
+
+			SetStyle(ControlStyles.StandardClick, false);
+		}
+
+		protected override void OnSizeChanged(EventArgs e)
+		{
+			Invalidate();
+			base.OnSizeChanged(e);
+		}
+
+		protected virtual Padding PressExpansion {
+			get {
+				return new Padding(1);
+			}
+		}
+
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			if (!Enabled)
+				PaintHelper.DrawDisabledFilter(e.Graphics, this);
+		}
+
+		protected override void OnPaintBackground(PaintEventArgs e)
+		{
+			Color parentBack = Parent.BackColor;
+			using (Brush brush = new SolidBrush(parentBack))
+				e.Graphics.FillRectangle(brush, ClientRectangle);
+		}
+
+		public int BorderWidth {
+			get;
+			set;
+		}
+
+		#region colors
+		public Color BorderColor {
+			get;
+			set;
+		}
+
+		public Color PressedBorderColor {
+			get;
+			set;
+		}
+
+		public Color PressedBackColor {
+			get;
+			set;
+		}
+
+		public Color PressedForeColor {
+			get;
+			set;
+		}
+		#endregion
+
+		#region IsPressed
+		private bool isPressed = false;
+
+		protected bool IsPressed {
+			get { return isPressed; }
+			set {
+				if (value != isPressed) {
+					isPressed = value;
+					Invalidate();
+				}
+			}
+		}
+		#endregion
+
+		#region mouse and key handling
+		protected override void OnMouseDown(MouseEventArgs e)
+		{
+			IsPressed = (e.Button == MouseButtons.Left);
+			base.OnMouseDown(e);
+		}
+
+		protected override void OnMouseMove(MouseEventArgs e)
+		{
+			if (!ClientRectangle.Contains(e.Location))
+				IsPressed = false;
+			base.OnMouseMove(e);
+		}
+
+		protected override void OnMouseUp(MouseEventArgs e)
+		{
+			if (IsPressed) {
+				IsPressed = false;
+				PerformClick();
+			}
+			base.OnMouseUp(e);
+		}
+
+		protected override void OnKeyDown(KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Enter || e.KeyCode == Keys.Space)
+				IsPressed = true;
+			base.OnKeyDown(e);
+		}
+
+		protected override void OnKeyUp(KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Enter || e.KeyCode == Keys.Space) {
+				IsPressed = false;
+				PerformClick();
+			}
+			base.OnKeyUp(e);
+		}
+		#endregion
+
+		#region IButtonControl
+		public DialogResult DialogResult {
+			get;
+			set;
+		}
+
+		public virtual void NotifyDefault(bool value)
+		{
+		}
+
+		public virtual void PerformClick()
+		{
+			OnClick(EventArgs.Empty);
+		}
+		#endregion
+	}
+}
+
