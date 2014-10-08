b0364811d5fdbd1193431faa1108d465a8896bdc
maul.esel <maul.esel@go4more.de>
2014-10-08 21:56:49 +0200

    add a custom timer class, TickOnceTimer

---
+++ TickOnceTimer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace VisualToolkit
+{
+	internal class TickOnceTimer : Timer
+	{
+		private bool ticked = false;
+
+		public bool EnsureOneTick {
+			get;
+			set;
+		}
+
+		public TickOnceTimer(int interval)
+			: base()
+		{
+			EnsureOneTick = true;
+			Interval = interval;
+		}
+
+		protected override void OnTick(EventArgs e)
+		{
+			ticked = true;
+			base.OnTick(e);
+		}
+
+		public new void Stop()
+		{
+			if (!ticked && EnsureOneTick)
+				OnTick(EventArgs.Empty);
+			ticked = false;
+			base.Stop();
+		}
+	}
+}
